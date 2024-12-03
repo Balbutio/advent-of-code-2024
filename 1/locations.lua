1871564f21851811212e1f91ebcc83931ab7c171
Nigel M <52312004+Balbutio@users.noreply.github.com>
2024-12-03 12:20:24 +0000

    day 2 initial commit

--- 1/locations.lua
+++ 1/locations.lua
@@ -1,20 +1,5 @@
--- http://lua-users.org/wiki/FileInputOutput
--- Return true if file exists and is readable.
-function file_exists(path)
-    local file = io.open(path, "rb")
-    if file then file:close() end
-        return file ~= nil
-end
-
--- http://lua-users.org/wiki/FileInputOutput
--- Read an entire file.
--- Use "a" in Lua 5.3; "*a" in Lua 5.1 and 5.2
-function read_all(filename)
-    local fh = assert(io.open(filename, "rb"))
-    local contents = assert(fh:read(_VERSION <= "Lua 5.2" and "*a" or "a"))
-    fh:close()
-    return contents
-end
+package.path = package.path .. ";../?.lua"
+require "file-utility"
 
 function split_lists(contents)
     local leftTable = {}
