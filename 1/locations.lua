46129cc88032a72341bbf0860a1fb9cdb4753e7a
Nigel M <52312004+Balbutio@users.noreply.github.com>
2024-12-03 00:04:41 +0000

    day 1, part 2

--- 1/locations.lua
+++ 1/locations.lua
@@ -25,27 +25,48 @@ function split_lists(contents)
         table.insert(rightTable, right)
         listLength = listLength + 1
     end
-
     return leftTable, rightTable, listLength
 end
 
+function calculate_distances(leftTable, rightTable, listLength)
+    local totalDistance = 0
+    local distance = 0
+    for i = 1, listLength, 1 do
+        distance = 0
+        if (leftTable[i] < rightTable[i]) then 
+            distance = rightTable[i] - leftTable[i]
+        elseif (leftTable[i] > rightTable[i]) then
+            distance = leftTable[i] - rightTable[i]
+        end
+        totalDistance = totalDistance + distance
+    end
+    return totalDistance
+end
+
+function calculate_similarity_score(leftTable, rightTable, listLength)
+    local totalSimilarity = 0
+    local occurrences = 0
+    for i = 1, listLength, 1 do
+        occurrences = 0
+        for x,y in pairs(rightTable) do
+            if(y == leftTable[i]) then
+                occurrences = occurrences + 1
+            end
+        end
+        totalSimilarity = totalSimilarity + (leftTable[i] * occurrences)
+    end
+    return totalSimilarity
+end
+
+local runType = arg[1]
 local file = "input.txt"
 local contents = read_all(file)
 local leftTable, rightTable, listLength = split_lists(contents)
 
-table.sort(leftTable)
-table.sort(rightTable)
-
-local totalGap = 0
-
-for i = 1, listLength, 1 do
-    local distance = 0
-    if (leftTable[i] < rightTable[i]) then 
-        distance = rightTable[i] - leftTable[i]
-    elseif (leftTable[i] > rightTable[i]) then
-        distance = leftTable[i] - rightTable[i]
-    end
-    totalGap = totalGap + distance
+if (runType == "1" or runType == "d" or runType == "distance") then
+    table.sort(leftTable)
+    table.sort(rightTable)
+    print(calculate_distances(leftTable, rightTable, listLength))
+elseif (runType == "2" or runType == "s" or runType == "similarity") then
+    print(calculate_similarity_score(leftTable, rightTable, listLength))
 end
-
-print(totalGap)
