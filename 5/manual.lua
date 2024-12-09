0007fc50a3c33a721c67e7eb4057f005a86315ca
Nigel M <52312004+Balbutio@users.noreply.github.com>
2024-12-09 23:44:37 +0000

    day 5, part 2

--- 5/manual.lua
+++ 5/manual.lua
@@ -1,10 +1,9 @@
 package.path = package.path .. ";../?.lua"
 require "file-utility"
 
-function get_update_lines(data)
+function get_lines(data)
     local ruleLines = {}
     local updateLines = {}
-
     for line in string.gmatch(data, "[^\r\n]+") do
         if (string.find(line, "|")) then
             table.insert(ruleLines, line)
@@ -41,29 +40,56 @@ function parse_updates(updateLines)
     return updates
 end
 
-function get_valid_update_count(rules, updates)
-    local validUpdateCount = 0
+function get_update_totals(rules, updates)
+    local validUpdateTotal = 0
+    local invalidUpdateTotal = 0
     for sequenceKey, updateSequence in pairs(updates) do
-        local previousUpdateValues = {}
-        local validSequence = true
-        for updateKey, updateValue in pairs(updateSequence) do
-            if (#previousUpdateValues ~= nil) then
-                if contains(rules, updateValue, previousUpdateValues) then
-                    validSequence = false
-                    break
-                end
+
+        local validSequence, errorIndex = test_sequence_validity(updateSequence, rules)
+        if (validSequence ~= nil) then
+            validUpdateTotal = validUpdateTotal + get_middle_value(validSequence)
+        elseif (errorIndex ~= nil) then
+            local newSequence = updateSequence
+            local previousSequence = {}
+            while ((validSequence == nil) and (previousSequence ~= newSequence)) do
+                newSequence = update_sequence_validity(newSequence, rules, errorIndex)
+                validSequence, errorIndex = test_sequence_validity(newSequence, rules)
             end
-            table.insert(previousUpdateValues, updateValue)
-        end
-        if (validSequence == true) then
-            targetIndex = ((math.floor(#previousUpdateValues/2))+1)
-            validUpdateCount = validUpdateCount + previousUpdateValues[targetIndex]
+            if (validSequence ~= nil) then
+                invalidUpdateTotal = invalidUpdateTotal + get_middle_value(validSequence)
+            end
         end
     end
-    return validUpdateCount
+    return validUpdateTotal, invalidUpdateTotal
 end
 
-function contains(table, key, previousUpdateValues)
+function get_middle_value(validSequence)
+    local targetIndex = ((math.floor(#validSequence/2))+1)
+    return validSequence[targetIndex]
+end
+
+function test_sequence_validity(updateSequence, rules)
+    local previousUpdateValues = {}
+    local validSequence = true
+    local errorIndex = nil
+    for updateIndex, updateValue in pairs(updateSequence) do
+        if (#previousUpdateValues ~= nil) then
+            if is_invalid_sequence(rules, updateValue, previousUpdateValues) then
+                validSequence = false
+                errorIndex = updateIndex
+                break
+            end
+        end
+        table.insert(previousUpdateValues, updateValue)
+    end
+    if (validSequence == true) then
+        return previousUpdateValues, nil
+    else
+        return nil, errorIndex
+    end
+end
+
+function is_invalid_sequence(table, key, previousUpdateValues)
     if (table[key] ~= nil) then
         for x, y in pairs(table[key]) do
             for pX, pY in pairs(previousUpdateValues) do
@@ -76,15 +102,28 @@ function contains(table, key, previousUpdateValues)
     return false
 end
 
+function update_sequence_validity(updateSequence, rules, errorIndex)
+    if (updateSequence[errorIndex - 1] ~= nil) then
+        updateSequence[errorIndex - 1], updateSequence[errorIndex] = updateSequence[errorIndex], updateSequence[errorIndex - 1]
+        return updateSequence
+    else 
+        return nil 
+    end
+end
+
 local ruleLines = {}
 local updateLines = {}
 local rules = {}
 local updates = {}
+local validUpdateTotal = 0
+local invalidUpdateTotal = 0
 
 local file = "input.txt"
 local contents = read_all(file)
-ruleLines, updateLines = get_update_lines(contents)
+ruleLines, updateLines = get_lines(contents)
 rules = parse_rules(ruleLines)
 updates = parse_updates(updateLines)
-validUpdateCount = get_valid_update_count(rules, updates)
-print(validUpdateCount)
+validUpdateTotal, invalidUpdateTotal = get_update_totals(rules, updates)
+print("Valid total: " .. validUpdateTotal)
+print("Invalid total: " .. invalidUpdateTotal)
+
